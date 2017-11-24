e3003a38c941c7b13c6a3281fa7c0af1024cf154
mgj <mgj@weyee.com>
2017-11-24 19:10:05 +0800

    自定义Toast

--- MHttp/mhttplib/src/main/java/com/mrmo/mhttplib/utils/MToast.java
+++ MHttp/mhttplib/src/main/java/com/mrmo/mhttplib/utils/MToast.java
@@ -2,8 +2,14 @@ package com.mrmo.mhttplib.utils;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.mrmo.mhttplib.R;
 import com.muddzdev.styleabletoastlibrary.StyleableToast;
 
 /**
@@ -11,7 +17,9 @@ import com.muddzdev.styleabletoastlibrary.StyleableToast;
  */
 
 public class MToast {
-    public static void show(Context context, String msg) {
+
+    @Deprecated
+    private static void showToast(Context context, String msg) {
         if (MStringUtil.isEmpty(msg)) {
             msg = "";
         }
@@ -22,6 +30,33 @@ public class MToast {
 //        st.setIcon(R.drawable.ic_autorenew_black_24dp);
         st.setMaxAlpha();
         st.show();
+    }
+
+    public static void show(Context context, String msg) {
+        View view = LayoutInflater.from(context).inflate(R.layout.http_toast_msg, null, false);
+
+        Toast toast = new Toast(context);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(view);
+
+        TextView tvMsg = (TextView) toast.getView().findViewById(R.id.tvMsg);
+        tvMsg.setText(msg);
+
+        int roundRadius = dp2px(context, 4);
+        GradientDrawable gd = new GradientDrawable();
+        gd.setAlpha(200);
+        gd.setColor(Color.parseColor("#000000"));
+        gd.setCornerRadius(roundRadius);
+        gd.setStroke(0, Color.parseColor("#000000"));
+        tvMsg.setBackgroundDrawable(gd);
+
+        toast.show();
+    }
+
+    private static int dp2px(Context context, final float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
     }
 
 }
